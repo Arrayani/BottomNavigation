355a2666c0d033e73d739c85d575322a08af5e6c
Roni <rampage9198@yahoo.com>
2022-05-06 15:48:04 +0700

    commit

--- app/src/main/java/com/ericg/searchview/bottomnavigation/MainActivity.kt
+++ app/src/main/java/com/ericg/searchview/bottomnavigation/MainActivity.kt
@@ -2,6 +2,7 @@ package com.ericg.searchview.bottomnavigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils.replace
 import android.view.View
 import com.gamingcode.FirstFragment
 import com.gamingcode.SecondFragment
@@ -36,12 +37,37 @@ class MainActivity : AppCompatActivity() {
 
             val profileButton = findViewById<View>(R.id.miProfile)
             profileButton.setOnClickListener{
-                supportFragmentManager.beginTransaction().apply {
-                    replace(R.id.flFragment, profileFragment)
-                    addToBackStack(null)
-                    commit()
-                }
-        }
 
-    }
-}}
+//                supportFragmentManager.beginTransaction().apply {
+//                        replace(R.id.flFragment, profileFragment)
+//                        addToBackStack(null)
+//                        commit()
+
+//                supportFragmentManager.beginTransaction().
+//                setCustomAnimations(
+//                    enter = R.anim.slide_in,
+//                    exit= R.anim.fade_out,
+//                    popEnter = R.anim.fade_in,
+//                    popExit =  R.anim.slide_out
+//                )
+//                replace(R.id.flFragment, profileFragment)
+//                addToBackStack(null)
+//                commit()
+//                }
+                val transactionx = supportFragmentManager.beginTransaction()
+                transactionx.setCustomAnimations(0,R.anim.fade_in
+                ).add(profileFragment,)
+
+//                (enter = R.anim.slide_in,exit= R.anim.fade_out,
+//                    popEnter = R.anim.fade_in,
+//                    popExit =  R.anim.slide_out)
+
+
+
+
+
+    }}
+}
+
+
+}
